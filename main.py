825caec6665f0508422cff6e7d20e1f75aad44df
princeBarimene <prince.g.olubari@gmail.com>
2024-11-14 13:40:30 +0000

    created the snake body with for loops from a list of tuples

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("Obec Snake game")
 
+#create a lsit of tuple for the positions
+starting_positions = [(0,0),(-20,0),(-40,0)]
+
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
+
+
+
+
 
 
 
