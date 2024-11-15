a442901e2c7536f2e4bfa9db616679cf6317d293
princeBarimene <prince.g.olubari@gmail.com>
2024-11-15 20:31:25 +0000

    detect collision with food

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600,height=600)
@@ -7,45 +10,28 @@ screen.bgcolor("black")
 screen.title("Obec Snake game")
 screen.tracer(0)
 
-#create a lsit of tuple for the positions
-starting_positions = [(0,0),(-20,0),(-40,0)]
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
-
-segments = []
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+    snake.move()
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num -1].xcor()
-        new_y = segments[seg_num -1].ycor()
-        segments[seg_num].goto(new_x,new_y)
-    segments[0].forward(20)
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 
 
