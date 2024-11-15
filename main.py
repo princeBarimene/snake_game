c029bd84b499b66ff32e140fecb1501d96fe363d
princeBarimene <prince.g.olubari@gmail.com>
2024-11-15 22:03:34 +0000

    completed snake game

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
         #detect collision with wall
@@ -36,6 +37,13 @@ while game_is_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.end_game()
+
+    for segment in snake.segments[1:]:
+       if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.end_game()
+
+
         
 
     
